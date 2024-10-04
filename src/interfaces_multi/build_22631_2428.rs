ce05fd25783bc11faee356470a6f9359ba7ed045
Lej77 <31554212+Lej77@users.noreply.github.com>
2024-10-04 03:44:39 +0200

    chore: comment change history of COM interfaces

--- src/interfaces_multi/build_22631_2428.rs
+++ src/interfaces_multi/build_22631_2428.rs
@@ -6,13 +6,14 @@
 //! > Valley 3") is the second major update to Windows 11. It was shipped as an
 //! > enablement package for Windows 11 2022 Update and carries the build number
 //! > 10.0.22631.
+
 use super::*;
 use build_22621_3155 as build_prev;
 
 build_prev::IApplicationView!("372E1D3B-38D3-42E4-A15B-8AB2B178F513");
 build_prev::IApplicationViewCollection!("1841C6D7-4F9D-42C0-AF41-8747538F10E5");
 build_prev::IVirtualDesktop!("3F07F4BE-B107-441A-AF0F-39D82529072C");
-build_prev::IVirtualDesktopManagerInternal!("A3175F2D-239C-4BD2-8AA0-EEBA8B0B138E"); // Changed
-build_prev::IVirtualDesktopNotification!("B287FA1C-7771-471A-A2DF-9B6B21F0D675"); // Changed
+build_prev::IVirtualDesktopManagerInternal!("A3175F2D-239C-4BD2-8AA0-EEBA8B0B138E"); // Changed (Same as build_22621_2215)
+build_prev::IVirtualDesktopNotification!("B287FA1C-7771-471A-A2DF-9B6B21F0D675"); // Changed (Same as build_22621_2215)
 build_prev::IVirtualDesktopNotificationService!("0cd45e71-d927-4f15-8b0a-8fef525337bf");
 build_prev::IVirtualDesktopPinnedApps!("4CE81583-1E4C-4632-A621-07A53543148F");
--- src/interfaces_multi/build_22631_3155.rs
+++ src/interfaces_multi/build_22631_3155.rs
@@ -4,7 +4,7 @@ use build_22631_2428 as build_prev;
 build_prev::IApplicationView!("372E1D3B-38D3-42E4-A15B-8AB2B178F513");
 build_prev::IApplicationViewCollection!("1841C6D7-4F9D-42C0-AF41-8747538F10E5");
 build_prev::IVirtualDesktop!("3F07F4BE-B107-441A-AF0F-39D82529072C");
-build_prev::IVirtualDesktopManagerInternal!("53F5CA0B-158F-4124-900C-057158060B27"); // Changed
-build_prev::IVirtualDesktopNotification!("B9E5E94D-233E-49AB-AF5C-2B4541C3AADE"); // Changed
+build_prev::IVirtualDesktopManagerInternal!("53F5CA0B-158F-4124-900C-057158060B27"); // Changed (Same as build_22621_3155)
+build_prev::IVirtualDesktopNotification!("B9E5E94D-233E-49AB-AF5C-2B4541C3AADE"); // Changed (Same as build_22621_3155)
 build_prev::IVirtualDesktopNotificationService!("0cd45e71-d927-4f15-8b0a-8fef525337bf");
 build_prev::IVirtualDesktopPinnedApps!("4CE81583-1E4C-4632-A621-07A53543148F");
